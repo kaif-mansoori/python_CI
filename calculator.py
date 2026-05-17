f8c2f20996d5e790ae92e0a0e7a0977d08e892df
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-05-17 09:57:26 +0000

    sybtax error4

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
-def add(a,b):
-    return a + b;
-def sub(a,b):
+def add(a, b):
+    return a + b; 
+
+def subtract(a, b):
     return a - b;
-def mul(a,b):
+
+def multiply(a, b):
     return a * b;
-def div(a,b):
+
+def divide(a, b):
     return a / b;
